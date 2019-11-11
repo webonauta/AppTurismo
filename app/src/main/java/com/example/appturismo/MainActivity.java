b404e445992b87e2291113a7c1f41eab0229fd33
Alberto Gabino <intrunet@gmail.com>
2019-11-10 22:49:56 -0600

    Subiendo vistas de Login y Registro

---
+++ app/src/main/java/com/example/appturismo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.appturismo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnRegistro = (Button) findViewById(R.id.btnRegistro);
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), RegistroActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/appturismo/RegistroActivity.java
@@ -0,0 +1,26 @@
+package com.example.appturismo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        Button btnIniciar = (Button) findViewById(R.id.btnIniciar);
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+}
